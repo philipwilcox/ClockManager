050ca8689bd903863cb7dc65cc29d6e29e8bab72
Philip Wilcox <philip@philipwilcox.com>
2018-02-01 14:23:52 -0500

    Add sample code for checking if there's a next alarm set in the system and printing it to the log if so

--- app/src/main/java/com/mopeyphil/clockmanager/MainActivity.kt
+++ app/src/main/java/com/mopeyphil/clockmanager/MainActivity.kt
@@ -2,11 +2,25 @@ package com.mopeyphil.clockmanager
 
 import android.app.Activity
 import android.os.Bundle
+import android.app.AlarmManager
+import android.content.Context
+import android.text.format.DateUtils
+import android.util.Log
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE);
+        var nextAlarm = (alarmManager as AlarmManager).nextAlarmClock
+        Log.e("AlarmManager: ", "$nextAlarm")
+        nextAlarm?.let {
+            var alarmString = DateUtils.formatDateTime(applicationContext, it.triggerTime,
+                    DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_TIME)
+            Log.e("AlarmManager", "Next alarm will trigger at $alarmString")
+        }
+
     }
 }
