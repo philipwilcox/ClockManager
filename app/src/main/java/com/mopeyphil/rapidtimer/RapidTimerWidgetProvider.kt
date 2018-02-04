9239165c3bd19cb6a59a5e27b6278c4c069834c1
Philip Wilcox <philip@philipwilcox.com>
2018-02-04 15:33:33 -0500

    Add a (not yet loadable) widget!

---
+++ app/src/main/java/com/mopeyphil/rapidtimer/RapidTimerWidgetProvider.kt
@@ -0,0 +1,9 @@
+package com.mopeyphil.rapidtimer
+
+import android.appwidget.AppWidgetProvider
+
+class RapidTimerWidgetProvider : AppWidgetProvider() {
+//    override fun onEnabled(context: Context) {
+//        Log.e("RapidTimer", "widget created!")
+//    }
+}
